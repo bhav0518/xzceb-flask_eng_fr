bafb85b1b2a107b300342e676779edbd0f4c8f4e
bhav0518@gmail.com <bhav0518@gmail.com>
2023-07-05 20:34:08 -0400

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -8,13 +8,13 @@ app = Flask("Web Translator", template_folder="templates")
 def renderIndexPage():
     return render_template('index.html')
 
-@app.route("/englishToFrench", methods=['POST'])
+@app.route("/englishToFrench", methods=['GET', 'POST'])
 def english_to_french_endpoint():
     english_text = request.args.get('textToTranslate')
     french_translation = translator.english_to_french(english_text)
     return french_translation
 
-@app.route("/frenchToEnglish", methods=['POST'])
+@app.route("/frenchToEnglish", methods=['GET', 'POST'])
 def french_to_english_endpoint():
     french_text = request.args.get('textToTranslate')
     english_translation = translator.french_to_english(french_text)
